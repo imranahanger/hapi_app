278438de69aa922bad99e002c6fa4d12303924d0
imranhanger <imranahanger@gmail.com>
2018-05-27 12:42:09 +0530

    add index.html and render that file

--- app.js
+++ app.js
@@ -15,17 +15,25 @@ server.route({
     }
 });
 
-server.route({
-    method:'GET',
-    path:'/{name}',
-    handler:(request,h)=>{
-        return `Hello, ${encodeURIComponent(request.params.name)}!`;
-    }
+// server.route({
+//     method:'GET',
+//     path:'/{name}',
+//     handler:(request,h)=>{
+//         return `Hello, ${encodeURIComponent(request.params.name)}!`;
+//     }
 
-})
+// })
 
 const init = async () => {
-
+    await server.register(require('inert'))
+    
+    server.route({
+        method:'GET',
+        path:'/hello',
+        handler:(request,h)=>{
+            return h.file('./public/index.html')
+        }
+    })
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
